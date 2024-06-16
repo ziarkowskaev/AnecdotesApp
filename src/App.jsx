42ce436db52bb63f1c4c3b23e1fee4ba05dbc362
Ewelina Ziarkowska <124273313+ziarkowskaev@users.noreply.github.com>
2024-06-16 19:29:32 +0300

    Commit 6.3-6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { createAnecdote, voteForA } from './reducers/anecdoteReducer'
+
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(content))
+  }
+
   const vote = (id) => {
-    console.log('vote', id)
+    dispatch(voteForA(id))
   }
 
   return (
@@ -23,9 +32,9 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnecdote}>
+        <div><input name='anecdote' /></div>
+        <button type='submit'>create</button>
       </form>
     </div>
   )
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -7,7 +7,7 @@ const anecdotesAtStart = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+export const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
   return {
@@ -19,11 +19,42 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  export const createAnecdote = (content) => {
+    return {
+      type: 'NEW_ANECDOTE',
+      payload: {
+        content,
+        id: getId(),
+        votes: 0
+        
+      }
+    }
+  }
 
-  return state
-}
+  export const voteForA = (id) => {
+    return {
+      type: 'VOTE',
+      payload: { id }
+    }
+  }
 
+  const reducer = (state = initialState, action) => {
+    console.log(action)
+    switch (action.type) {
+      case 'NEW_ANECDOTE':
+        return [...state, action.payload]
+      case 'VOTE':
+        const id = action.payload.id
+        const anecdoteToChange = state.find(n => n.id === id)
+        const changedAnecdote = { 
+          ...anecdoteToChange, 
+          votes: anecdoteToChange.votes + 1
+        }
+        return state.map(anecdote =>
+          anecdote.id !== id ? anecdote : changedAnecdote
+        )
+      default:
+        return state
+      }
+    }
 export default reducer
