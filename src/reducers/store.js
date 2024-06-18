3fcaa115367d05046c21c4ee872765315ec0a39e
Ewelina Ziarkowska <124273313+ziarkowskaev@users.noreply.github.com>
2024-06-18 20:48:31 +0300

    Commit part6.14-15
    Anecdotes are stored and saved in the backend server.

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
-import anecdoteReducer from "./anecdoteReducer"
+import anecdoteReducer, {setAnecdotes} from "./anecdoteReducer"
 import filterReducer from "./filterReducer"
 import notificationReducer from "./notificationReducer"
+import anecdoteService from "../services/anecdotes"
 
 const store = configureStore({
   reducer: {
@@ -10,5 +11,10 @@ const store = configureStore({
     notification: notificationReducer,
   },
 })
+
+anecdoteService.getAll().then(anecdotes =>
+  store.dispatch(setAnecdotes(anecdotes))
+  )
+
 
 export default store
